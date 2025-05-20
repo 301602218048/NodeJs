f74ab4cdc03f726548e9ecfe3bbf98e5df730487
301602218048 <abhinav.sharma2901@gmail.com>
2025-05-20 16:29:11 +0530

    middleware

--- 03Express/app.js
+++ 03Express/app.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
+app.use("/welcome", (req, res, next) => {
+  req.user = "Guest";
+  next();
+});
+app.get("/welcome", (req, res) => {
+  res.send(`<h1>Welcome, ${req.user}</h1>`);
+});
+
 app.listen(3000, () => {
   console.log(
     "Server is up and running on port 3000! Ready to handle requests."
