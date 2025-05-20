9b2dae65e74206a785e03aa7e4ea3cf5d514f9a9
301602218048 <abhinav.sharma2901@gmail.com>
2025-05-20 21:18:47 +0530

    middleware

--- 03Express/app.js
+++ 03Express/app.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
 
-app.get("/welcome/:username", (req, res) => {
-  const name = req.params.username;
-  const role = req.query.role;
-  res.send(`Welcome ${name}, your role is ${role}`);
+app.use((req, res, next) => {
+  console.log(req.method, "request made to", req.url);
+  next();
 });
 
-app.use((req, res) => {
-  res.status(404).send(`<h1>404-Page Not Found</h1>`);
+app.get("/products", (req, res) => {
+  res.send("Here is the list of all products.");
+});
+
+app.get("/categories", (req, res) => {
+  res.send("Here is the list of all categories.");
+});
+
+app.post("/products", (req, res) => {
+  res.send("A new product has been added.");
+});
+
+app.post("/categories", (req, res) => {
+  res.send("A new category has been created.");
 });
 
 const port = 4000;
