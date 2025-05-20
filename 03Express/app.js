bc0afac1e3ae5e50e106017649d6d36815b437c7
301602218048 <abhinav.sharma2901@gmail.com>
2025-05-20 19:45:26 +0530

    random endpoint

--- 03Express/app.js
+++ 03Express/app.js
@@ -17,7 +17,11 @@ app.post("/users", (req, res) => {
   res.send("A new user has been added.");
 });
 
-const port = 3000;
+app.use((req, res) => {
+  res.status(404).send(`<h1>404-Page Not Found</h1>`);
+});
+
+const port = 4000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
