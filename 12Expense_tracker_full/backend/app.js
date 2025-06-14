cb06ba02c39096f6ec46bd186df28af6bf45efcd
301602218048 <abhinav.sharma2901@gmail.com>
2025-06-14 14:49:54 +0530

    morgan implement

--- 12Expense_tracker_full/backend/app.js
+++ 12Expense_tracker_full/backend/app.js
@@ -6,7 +6,10 @@ const paymentRoutes = require("./routes/paymentRoutes");
 const PremiumRoutes = require("./routes/premiumRoutes");
 const passwordRoutes = require("./routes/passwordRoutes");
 const path = require("path");
+const fs = require("fs");
+const morgan = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
 //models
 require("./models");
@@ -15,7 +18,13 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "..", "frontend")));
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
 app.use(cors());
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use(express.json());
 app.use("/user", userRoutes);
 app.use("/premium", PremiumRoutes);
@@ -23,7 +32,7 @@ app.use("/pay", paymentRoutes);
 app.use("/expenses", expenseRoutes);
 app.use("/password", passwordRoutes);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 db.sync({ alter: false })
   .then(() => {
     app.listen(port, () => {
