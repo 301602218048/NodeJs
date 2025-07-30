6d1c172ca798de034c8d19b745975a80432b8d7a
301602218048 <abhinav.sharma2901@gmail.com>
2025-07-30 18:15:44 +0530

    ...

--- 14Nosql/01NoSQL/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { addUser, findUserById } = require("../controllers/userController");
-
-const router = express.Router();
-
-router.post("/addUser", addUser);
-router.get("/getUser/:id", findUserById);
-
-module.exports = router;
